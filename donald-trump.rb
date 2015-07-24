802ebeca94bb788db043198db8ba376976fcb8be
Daniel Fein <danieljfein@gmail.com>
2015-07-24 05:18:06 -0400

    attr_accessor

--- donald-trump.rb
+++ donald-trump.rb
@@ -1,45 +1,52 @@
 require './math.rb'
 
 class Candidate
+attr_accessor :age,:name
+
   def initialize(party)
     if(party == 1) then
-    @party = "Democrat"
-  else if(party == 2) then
-    @party = "Republican"
-  else
-    @party = "Independent"
-  end end
+      @party = "Democrat"
+    elsif(party == 2) then
+      @party = "Republican"
+    else
+      @party = "Independent"
+    end
+
   end
-  def name=(name)
+  def name(name)
     @name = name
   end
   def age=(age)
     @age = age
   end
-  def name
-    @name
-  end
-  def age
-    @age
-  end
   def party
     @party
   end
+  # def name
+  #   @name
+  # end
+  # def age
+  #   @age
+  # end
+  # def party
+  #   @party
+  # end
 end
 
 =begin
 donald = Candidate.new.name=("Donald Trump").age=("not sure")
-      this doesn't work because it is calling age on name
+this doesn't work because it is calling age on name
 =end
 
 
 donald = Candidate.new(2)
-donald.name=("Donald Trump")
+donald.name("Donald Trump")
 donald.age=("68")
-
+puts donald.party
+puts donald.age;
 math_object = EasyMath.new
 new_age = math_object.multiply(donald.age,2)
 
-puts donald.party,donald.name, donald.age
-puts donald.name+" is a "+donald.age+ " year old "+donald.party
-puts donald.name+"'s age multiplied by 2 is " + new_age.to_s
+# puts donald.party,donald.name, donald.age
+# puts donald.name+" is a "+donald.age+ " year old "+donald.party
+# puts donald.name+"'s age multiplied by 2 is " + new_age.to_s
